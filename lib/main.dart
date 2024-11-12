75b1f821259da913a67cb017f71d6e8972b957d2
JackFrostQ <nikowijaya110@gmail.com>
2024-11-12 12:45:46 +0700

    Pengerjaan tugas wisata candi terbaru

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:wisata_candi_nicowijaya/data/candi_data.dart';
+import 'package:wisata_candi_nicowijaya/models/candi.dart';
+import 'package:wisata_candi_nicowijaya/screen/lib/screens/profile_screen.dart';
+import 'package:wisata_candi_nicowijaya/screen/lib/screens/search_screen.dart';
+import 'package:wisata_candi_nicowijaya/screen/lib/screens/sign_in_screen.dart';
+import 'package:wisata_candi_nicowijaya/screen/lib/screens/sign_up_screen.dart';
+import 'package:wisata_candi_nicowijaya/screen/lib/screens/home_screen.dart';
+
 
 void main() {
   runApp(const MyApp());
@@ -13,20 +21,26 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Wisata Candi',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        appBarTheme: const AppBarTheme(
+          iconTheme: IconThemeData(color: Colors.deepPurple),
+          titleTextStyle: TextStyle(
+            color: Colors.deepPurple,
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple).copyWith(
+          primary: Colors.deepPurple,
+          surface: Colors.deepPurple[50],
+        ),
         useMaterial3: true,
       ),
-      home: DetailScreen(),
+      // home: ProfileScreen(),
+      // home: DetailScreen(candi: candiList[0]),
+      // home: SignInScreen(),
+      // home: SignUpScreen(),
+      // home: SearchScreen(),
+      home: HomeScreen(),
     );
-  }
-}
-
-class DetailScreen extends StatelessWidget {
-  const DetailScreen({super.key,});
-
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
-
   }
 }
